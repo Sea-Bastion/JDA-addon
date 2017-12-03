5cd51ecc13ae9a52d9d1c11c951f49b80d3b409a
Sebastian Cypert <Sebastian.Cypert@gmail.com>
2017-12-02 23:58:53 -0600

    need to work on the send func, input not working at all, and need a output customozer thing

--- src/main/java/bots/JDAAddon/CJDAImpl.java
+++ src/main/java/bots/JDAAddon/CJDAImpl.java
@@ -7,13 +7,30 @@ import okhttp3.OkHttpClient;
 
 public class CJDAImpl extends JDAImpl implements CJDA {
 
-	Input input;
+	private Input input;
 
 	public CJDAImpl(AccountType accountType, OkHttpClient.Builder httpClientBuilder, WebSocketFactory wsFactory, boolean autoReconnect, boolean audioEnabled, boolean useShutdownHook, boolean bulkDeleteSplittingEnabled, int corePoolSize, int maxReconnectDelay) {
 		super(accountType, httpClientBuilder, wsFactory, autoReconnect, audioEnabled, useShutdownHook, bulkDeleteSplittingEnabled, corePoolSize, maxReconnectDelay);
-		new Thread(input = new Input(this)).start();
+
+		new Thread(this::WaitForInput).start();
+	}
+
+
+	private void WaitForInput() {
+		while(this.getStatus() != Status.CONNECTED) {
+			try {
+				Thread.sleep(50L);
+
+			} catch (InterruptedException e) {
+				System.err.println("failed to wait for CJDA to log in");
+				System.exit(1);
+			}
+		}
+
+		new Thread(input = new Input(this));
 		this.addEventListener(input);
 	}
+
 
 	@Override
 	public Input getInput() {
